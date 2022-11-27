cf3505575c765609f44acd5e8b2bcca54f94a3b4
m-riss <95452643+m-riss@users.noreply.github.com>
2022-11-27 18:54:11 -0500

    Create Compilation Script.bat
    Compiles the code automatically if you don't want to type out the actual command. Change the path on line 1 to match your path/directory

---
+++ Compilation Script.bat
@@ -0,0 +1,8 @@
+cd C:\Users\User\Downloads\Minecraft-Seed-Converter-main
+g++ -S minecraft_seed_converter.cpp
+g++ -s minecraft_seed_converter.cpp -oSeed_Converter_O0 -g
+g++ -s minecraft_seed_converter.cpp -oSeed_Converter_O1 -O1
+g++ -s minecraft_seed_converter.cpp -oSeed_Converter_O2 -O2
+g++ -s minecraft_seed_converter.cpp -oSeed_Converter_O3 -O3
+g++ -s minecraft_seed_converter.cpp -oSeed_Converter_Os -Os
+g++ -s minecraft_seed_converter.cpp -oSeed_Converter_Ofast -Ofast
